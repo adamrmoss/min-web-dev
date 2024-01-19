aa5857c95e7e46ec78258cf450006935f66ab799
Adam R Moss <adamrmoss@gmail.com>
2024-01-19 14:25:44 -0500

    DateTimeExtensions

--- MinWebDev.Web/Sources/Resume/ResumeSectionTagHelper.cs
+++ MinWebDev.Web/Sources/Resume/ResumeSectionTagHelper.cs
@@ -8,23 +8,29 @@ public class ResumeSectionTagHelper : TagHelper
 {
     public required string Title { get; set; }
     
-    public override void Process(TagHelperContext context, TagHelperOutput output)
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
-        var innerContent = output.GetChildContentAsync().Result.GetContent();
+        // Capture innerContent
+        var innerContent = await output.GetChildContentAsync();
 
+        // Build sectionTitleHeading
         var sectionTitleHeading = new TagBuilder("h2");
         sectionTitleHeading.AddCssClass("resume-section-title");
         sectionTitleHeading.InnerHtml.Append(this.Title);
 
+        // Build sectionBody
         var sectionBody = new TagBuilder("div");
         sectionBody.AddCssClass("resume-section-body");
         sectionBody.InnerHtml.AppendHtml(innerContent);
 
-        var section = new TagBuilder("section");
-        section.AddCssClass("resume-section");
-        section.InnerHtml.AppendHtml(sectionTitleHeading);
-        section.InnerHtml.AppendHtml(sectionBody);
+        // Build section
+        var section1 = new TagBuilder("section");
+        section1.AddCssClass("resume-section");
+        section1.InnerHtml.AppendHtml(sectionTitleHeading);
+        section1.InnerHtml.AppendHtml(sectionBody);
+        var section = section1;
 
+        // Only render the section
         output.TagMode = TagMode.StartTagAndEndTag;
         output.SuppressOutput();
         output.PreContent.SetHtmlContent(section);
