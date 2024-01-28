413ced90ada33e5335c6b719ce45199fea23b626
Adam R Moss <adamrmoss@gmail.com>
2024-01-28 11:26:30 -0500

    initialize-settings.ts

---
+++ initialize-settings.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import { glob } from 'glob'
+
+glob('**/sample.*settings.json')
+    .then(sampleSettingsFilenames => {
+        for (const sampleSettingsFilename of sampleSettingsFilenames)
+        {
+            const settingsFilename = sampleSettingsFilename.replace('sample.', '');            
+            const fileExists = fs.existsSync(settingsFilename);
+            
+            if (fileExists) {
+                console.log(`Settings file: ${settingsFilename} already exists.  Skipping.`);
+            } else {
+                console.log(`Copying ${sampleSettingsFilename} to ${settingsFilename}.`);
+                fs.copyFileSync(sampleSettingsFilename, settingsFilename, fs.constants.COPYFILE_EXCL);
+            }
+        }        
+    });
